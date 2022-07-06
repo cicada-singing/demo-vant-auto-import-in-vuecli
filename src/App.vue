cd7c075b3046d274700ab345f282953453199103
知了凉亭轻声叫 <foryli@163.com>
2022-07-06 15:14:43 +0800

    wip: init base development environment

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>演示 Vant 3 基础组件</h1>
+
+  <h2>Button 按钮</h2>
+
+  <van-button type="primary">主要按钮</van-button>
+  <van-button type="success">成功按钮</van-button>
+  <van-button type="default">默认按钮</van-button>
+  <van-button type="warning">警告按钮</van-button>
+  <van-button type="danger">危险按钮</van-button>
+
+  <h2>Icon 图标</h2>
+
+  <van-icon name="chat-o" />
+  <van-icon dot name="chat-o" />
+  <van-icon badge="9" name="chat-o" />
+  <van-icon color="#1989fa" name="cart-o" />
+  <van-icon color="#ee0a24" name="fire-o" />
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require("@vue/cli-service");
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { VantResolver } = require("unplugin-vue-components/resolvers");
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: (config) => {
+    config.plugins.push(
+      ...[
+        AutoImport({
+          resolvers: [VantResolver()],
+          dts: path.resolve(process.cwd(), "auto-imports.d.ts")
+        }),
+        Components({
+          resolvers: [VantResolver()],
+          dts: path.resolve(process.cwd(), "components.d.ts")
+        })
+      ]
+    );
+  }
 });
